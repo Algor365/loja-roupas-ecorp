f67cd00482e3742e701b59971345d48700c9f297
Algor365 <higor.vieira965@gmail.com>
2025-11-19 17:11:30 -0300

    Adicionar arquivo nov

--- loja_roupas/core/urls.py
+++ loja_roupas/core/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login_view, name='login'),
+    path('login/', views.login_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
+    path('register/', views.register_view, name='login'),
     path('api/clientes/', views.api_clientes, name='api_clientes'),
     path('api/fornecedores/', views.api_fornecedores, name='api_fornecedores'),
     path('api/produtos/', views.api_produtos, name='api_produtos'),
